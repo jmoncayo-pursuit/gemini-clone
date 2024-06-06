f7b8724f5e1c5401fff98e2405e4b14aef160a2c
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-06-05 20:34:12 -0400

    Api linking Update

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,9 +13,9 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const API = import.meta.env.VITE_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-const genAI = new GoogleGenerativeAI(API);
+const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
